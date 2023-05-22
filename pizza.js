8e3743f8098eaf8ddeffe0bd10f07b320fbfe5aa
NataliaGM1 <ngmihalache@gmail.com>
2023-05-22 15:20:06 +0200

    niveling main branch

---
+++ pizza.js
@@ -0,0 +1,22 @@
+console.log("Receta pizza verde");
+
+const dough= "Masa Fresca"
+const basilpesto = "Pesto de Albahaca 30 gr"
+const broccoli ="Brocoli"
+const zucchini ="Calabacín"
+const bluecheese ="Queso Azúl 30 gr"
+const mozzarellacheese ="Queso Mozzarella 30 gr"
+const freshoregano="Oregano Fresco"
+
+const ingredients = ["Dought", "Pesto albahaca", "Brocoli", "Calabacin", "Queso Azúl", "Mozzarella", "Orégano"]
+
+
+console.log("1. Precalienta el horno a 180°C.");
+console.log("2. Extiende la " + dough + " sobre una superficie enharinada hasta obtener el tamaño deseado.");
+console.log("3. Coloca la masa en una bandeja para pizza.");
+console.log("4. Unta la " + basilpesto + " sobre la masa de pizza.");
+console.log("5. Espolvorea generosamente el " + bluecheese + " sobre la salsa.");
+console.log("6. Agrega el " + broccoli + " );
+console.log("7. Hornea la pizza en el horno precalentado durante 15-20 minutos o hasta que la masa esté dorada y el queso esté burbujeante.");
+console.log("8. Retira del horno y deja enfriar durante unos minutos antes de cortarla en porciones.");
+console.log("9. ¡Ya tienes tu deliciosa pizza verde casera!");   
